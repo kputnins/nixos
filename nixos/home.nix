e7cceac7feb67e58a047085895201db010d57905
kputnins <kaspars.putnins19@gmail.com>
2024-03-12 22:16:21 +0200

    removed comments

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "kp";
   home.homeDirectory = "/home/kp";
 
@@ -17,23 +15,7 @@
 
   # The home.packages option allows you to install Nix packages into your environment.
   home.packages = [
-    # Adds the 'hello' command to your environment. It prints a friendly
-    # "Hello, world!" when run.
-    pkgs.hello
     pkgs.dconf
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   programs.zsh = {
@@ -114,10 +96,6 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-    # NOTE: disabled, because causes breakage often
-    /* config.global = {
-      strict_env = true;
-    }; */
   };
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -135,21 +113,6 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/kp/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
     EDITOR = "micro";
     SUDO_EDITOR = "micro";
