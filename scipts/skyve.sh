4a70dd5cffd765ff944a990f451ff0ec4b585f44
kputnins <kaspars.putnins19@gmail.com>
2024-07-10 21:53:19 +0300

    feat: added skyve launching script

---
+++ scipts/skyve.sh
@@ -0,0 +1,6 @@
+STEAM_ID=949230
+
+STEAM_DIR="$HOME/.local/share/Steam"
+SKYVE_PATH="steamapps/compatdata/$STEAM_ID/pfx/drive_c/Program Files (x86)/Skyve CS-II/Skyve.exe"
+
+protontricks-launch --appid $STEAM_ID  "$STEAM_DIR/$SKYVE_PATH"
