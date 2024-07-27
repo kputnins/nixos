8590f57ae5e2226097acdc8eb03cf6745cf71071
kputnins <kaspars.putnins19@gmail.com>
2024-07-27 12:50:13 +0300

    chore: updated packages and lock file

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -10,7 +10,7 @@
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "nct6775" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
